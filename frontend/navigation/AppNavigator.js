f3957b916808b9fc216db7985896871c1bb84318
Caboia <caio.rodriguesyokoyama@gmail.com>
2024-09-13 10:26:35 -0300

    :sparkles: feat: Tela de login

---
+++ frontend/navigation/AppNavigator.js
@@ -0,0 +1,23 @@
+// /navigation/AppNavigator.js
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import LoginScreen from '../screens/LoginScreen';
+
+const Stack = createStackNavigator();
+
+const AppNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ headerShown: false }} 
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default AppNavigator;
---
+++ frontend/screens/LoginScreen.js
@@ -0,0 +1,119 @@
+import React, { useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Image } from 'react-native';
+import { Ionicons } from '@expo/vector-icons'; 
+
+export default function App() {
+  
+  const [passwordVisible, setPasswordVisible] = useState(false); 
+
+  const togglePasswordVisibility = () => {
+    setPasswordVisible(!passwordVisible);
+  };
+
+  return (
+    <View style={styles.container}>
+
+<Image
+        style={styles.stretch}
+        source={require('../assets/weather.png')}
+      />
+      
+      <Text style={styles.title}>ClimaMonitor</Text>
+      
+      <View style={styles.inputContainer}>
+        <Ionicons name="mail-outline" size={20} color="#666" style={styles.inputIcon} />
+        <TextInput
+          style={styles.input}
+          placeholder="e-mail"
+          placeholderTextColor="#999"
+        />
+      </View>
+      
+      <View style={styles.inputContainer}>
+        <Ionicons name="lock-closed-outline" size={20} color="#666" style={styles.inputIcon} />
+        <TextInput
+          style={styles.input}
+          placeholder="Senha"
+          placeholderTextColor="#999"
+          secureTextEntry={!passwordVisible}
+        />
+         <TouchableOpacity onPress={togglePasswordVisibility}>
+          <Ionicons 
+            name={passwordVisible ? "eye-off-outline" : "eye-outline"} 
+            size={20} 
+            color="#666" 
+            style={styles.eyeIcon} 
+          />
+        </TouchableOpacity>
+      </View>
+      
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Entrar</Text>
+      </TouchableOpacity>
+      <Text style={styles.linkText}>Primeiro acesso</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+ 
+  container: {
+    flex: 1,
+    backgroundColor: '#f5f5f5',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 20,
+  },
+
+  title: {
+    marginTop: 20,
+    fontSize: 32,
+    fontWeight: 'bold',
+    color: '#333',
+    marginBottom: 40,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    borderColor: '#ddd',
+    borderRadius: 8,
+    paddingHorizontal: 10,
+    paddingVertical: 8,
+    marginBottom: 15,
+    width: '100%',
+  },
+  inputIcon: {
+    marginRight: 10,
+  },
+  input: {
+    flex: 1,
+    fontSize: 16,
+    color: '#333',
+  },
+  eyeIcon: {
+    marginLeft: 10,
+  },
+  button: {
+    backgroundColor: '#4da6ff',
+    paddingVertical: 12,
+    paddingHorizontal: 25,
+    borderRadius: 8,
+    width: '100%',
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  linkText: {
+    color: '#4da6ff',
+    marginTop: 20,
+    textDecorationLine: 'none',
+  },
+});
