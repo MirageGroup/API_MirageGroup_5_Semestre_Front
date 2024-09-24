24047090668ce6ec25a188957bc0b862564b42e4
Caboia <caio.rodriguesyokoyama@gmail.com>
2024-09-24 12:11:59 -0300

    :sparkles: feat: Integração com back

--- frontend/src/components/Slider.tsx
+++ frontend/src/components/Slider.tsx
@@ -1,44 +1,41 @@
-import MultiSlider from '@ptomasroos/react-native-multi-slider';
-import * as React from 'react';
-import { useState } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
-
+import MultiSlider from "@ptomasroos/react-native-multi-slider";
+import * as React from "react";
+import { useState } from "react";
+import { Text, View, StyleSheet } from "react-native";
 
 interface SliderProps {
-    min?: number;
-    max?: number;
-    step?: number;
-    initialMinValue?: number;
-    initialMaxValue?: number;
-    onValuesChange?: (values: number[]) => void;
-    labelLeft?: string;
-    labelRight?: string;
-    unit?: string;
+  min?: number;
+  max?: number;
+  step?: number;
+  initialMinValue?: number;
+  initialMaxValue?: number;
+  onValuesChange?: (values: number[]) => void;
+  labelLeft?: string;
+  labelRight?: string;
+  unit?: string;
 }
 
 const Slider: React.FC<SliderProps> = ({
-    min = 0,
-    max = 100,
-    step = 1,
-    initialMinValue = min,
-    initialMaxValue = max,
-    onValuesChange,
-    labelLeft = 'Min',
-    labelRight = 'Max',
-    unit = '',
+  min = 0,
+  max = 100,
+  step = 1,
+  initialMinValue = min,
+  initialMaxValue = max,
+  onValuesChange,
+  labelLeft = "Min",
+  labelRight = "Max",
+  unit = "",
 }) => {
-    const [minValue, setMinValue] = useState(initialMinValue);
-    const [maxValue, setMaxValue] = useState(initialMaxValue);
+  const [minValue, setMinValue] = useState(initialMinValue);
+  const [maxValue, setMaxValue] = useState(initialMaxValue);
 
-    const handleValuesChange = (values: number[]) => {
-        setMinValue(values[0]);
-        setMaxValue(values[1]);
-        if(onValuesChange) {
-            onValuesChange(values);
-        }
-    };
-
-    
+  const handleValuesChange = (values: number[]) => {
+    setMinValue(values[0]);
+    setMaxValue(values[1]);
+    if (onValuesChange) {
+      onValuesChange(values);
+    }
+  };
 
   return (
     <View style={styles.sliderContainer}>
@@ -51,77 +48,82 @@ const Slider: React.FC<SliderProps> = ({
         min={min}
         max={max}
         step={step}
-        sliderLength={330}
+        sliderLength={290}
         onValuesChange={handleValuesChange}
         selectedStyle={styles.sliderFront}
         unselectedStyle={styles.sliderBack}
         customMarker={() => <View style={styles.thumb} />}
-        />
+      />
       <View style={styles.values}>
         <View style={styles.valueContainer}>
-            <Text style={styles.valueText}>{minValue}{unit}</Text>
+          <Text style={styles.valueText}>
+            {minValue}
+            {unit}
+          </Text>
         </View>
         <View style={styles.valueContainer}>
-            <Text style={styles.valueText}>{maxValue}{unit}</Text>
+          <Text style={styles.valueText}>
+            {maxValue}
+            {unit}
+          </Text>
         </View>
       </View>
     </View>
-    
   );
 };
 
 export default Slider;
 
 const styles = StyleSheet.create({
-    sliderContainer:{
-        justifyContent: 'center',
-        alignSelf: 'center',
-    },
-    labels: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        marginBottom: -8,
-    },
-    label: {
-        fontSize: 13,
-        color: '#3874CF',
-        fontWeight: '300',
-    },
-    sliderFront: {
-        height: 12,
-        backgroundColor: '#3874CF',
-        borderRadius: 20,
-    },
-    sliderBack: {
-        height: 12,
-        backgroundColor: '#DDDDDD',
-        borderRadius: 20,
-    },
-    thumb: {
-        marginTop: 10,
-        height: 25,
-        width: 25,
-        borderRadius: 12.5,
-        backgroundColor: '#4A90E2',
-        borderWidth: 4,
-        borderColor: '#FFF',
-        elevation: 4,
-    },
-    values: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-    },
-    valueText: {
-        color: '#000000',
-        width: 45,
-        height: 25,
-        textAlignVertical: 'center',
-        textAlign: 'center',
-        fontWeight: '300',
-        fontSize: 13,
-    },
-    valueContainer: {
-        backgroundColor: '#DDDDDD',
-        borderRadius: 5,
-    },
+  sliderContainer: {
+    justifyContent: "center",
+    alignSelf: "center",
+  },
+  labels: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: -8,
+  },
+  label: {
+    fontSize: 13,
+    color: "#3874CF",
+    fontWeight: "300",
+  },
+  sliderFront: {
+    height: 12,
+    backgroundColor: "#3874CF",
+    borderRadius: 20,
+  },
+  sliderBack: {
+    height: 12,
+    backgroundColor: "#DDDDDD",
+    borderRadius: 20,
+  },
+  thumb: {
+    marginTop: 10,
+    height: 25,
+    width: 25,
+    borderRadius: 12.5,
+    backgroundColor: "#4A90E2",
+    borderWidth: 4,
+    borderColor: "#FFF",
+    elevation: 4,
+  },
+  values: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  valueText: {
+    color: "#000000",
+    width: 45,
+    height: 25,
+    textAlignVertical: "center",
+    textAlign: "center",
+    fontWeight: "300",
+    fontSize: 13,
+  },
+  valueContainer: {
+    backgroundColor: "#DDDDDD",
+    borderRadius: 5,
+  },
 });
