2e1c060294130f79584832b773a0fcb038e6a20c
Jacqueline Mariane Bezerra <jacquelinemrnbmp@outlook.com>
2024-09-27 15:32:18 -0300

    Navbar

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,14 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Navbar from './src/components/Navbar';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Template Inicial Frontend Mirage API 5</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Navbar />
   );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
