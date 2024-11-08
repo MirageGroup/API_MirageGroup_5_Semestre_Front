6a6c55cd37e6f107f50ee4be7da88c2ea639c598
Gisele Reis <giselealvesdosreis@gmail.com>
2024-11-08 15:29:47 -0300

    :sparkles: feat: save user id

--- frontend/src/screens/LoginScreen.tsx
+++ frontend/src/screens/LoginScreen.tsx
@@ -11,6 +11,7 @@ import {
 } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { API_URL } from "@env";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 interface LoginProps {
   onLogin: () => void;
@@ -44,6 +45,8 @@ const Login: React.FC<LoginProps> = ({ onLogin }) => {
       });
 
       if (response.status === 200) {
+        const data = await response.json();
+        await AsyncStorage.setItem('userId', data.toString());
         onLogin();
       } else if (response.status === 401) {
         Alert.alert("Erro", "Credenciais inválidas.");
